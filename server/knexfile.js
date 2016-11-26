27f7f9bf8cba99bbaa5c264f1568de464ac7aa94
MrCooper42 <mr.cooper42co@gmail.com>
2016-11-26 12:43:14 -0700

    knex.js .env now node_env

--- server/knexfile.js
+++ server/knexfile.js
@@ -12,6 +12,9 @@ module.exports = {
 	production: {
 		client: `pg`,
 		connection: process.env.DATABASE_URL,
+		migrations: {
+			directory: `./migrations`
+		}
 		seeds: {
 			directory: `./seeds`
 		}
